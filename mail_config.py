ab404469621d9c7cc2294fa768fead283935e71a
faten khalfa <faten.khalfa@isimg.tn>
2019-11-15 13:46:11 +0100

    security_camera_balena

--- mail_config.py
+++ mail_config.py
@@ -4,13 +4,13 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
 # Email you want to send the update from (only works with gmail)
-fromEmail = 'faten.khalfa@isimg.tn'
+fromEmail = 'tape your sender e-mail'
 # You can generate an app password here to avoid storing your password in plain text
 # https://support.google.com/accounts/answer/185833?hl=en
-fromEmailPassword = 'allahALLAH123'
+fromEmailPassword = 'tape your password'
 
 # Email you want to send the update to
-toEmail = 'm.issam.limem@gmail.com'
+toEmail = 'tape your receiver e-mail'
 
 def sendEmail(image):
 	msgRoot = MIMEMultipart('related')
